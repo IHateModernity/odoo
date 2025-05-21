ecc76e8ed18a8246f45208835a13017e57e92fc9
Mark <markmurpheen@gmail.com>
2025-05-21 19:06:40 +0300

    Test
    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,5 +25,5 @@ exec odoo \
     --smtp-port="${ODOO_SMTP_PORT_NUMBER}" \
     --smtp-user="${ODOO_SMTP_USER}" \
     --smtp-password="${ODOO_SMTP_PASSWORD}" \
-    --addons-path=/usr/lib/python3/dist-packages/odoo/addons,/app/addons \
+    --addons-path=/usr/lib/python3/dist-packages/odoo/addons \
     --email-from="${ODOO_EMAIL_FROM}" 2>&1
