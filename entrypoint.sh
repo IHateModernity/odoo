83947fb39d753bf9fd044cf62ce07b5ed1f4bfe4
Mark <markmurpheen@gmail.com>
2025-05-22 17:58:25 +0300

    W
    W

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,6 @@ exec odoo \
      $ODOO_INIT \
     --without-demo=True \
     --proxy-mode \
-    --workers=2 \
-    --max-cron-threads=1 \
     --db_host="${ODOO_DATABASE_HOST}" \
     --db_port="${ODOO_DATABASE_PORT}" \
     --db_user="${ODOO_DATABASE_USER}" \
