50b60af5128120704c69833852213e94fc503c83
Mark <markmurpheen@gmail.com>
2025-05-22 18:01:54 +0300

    T
    T

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,14 @@ echo "Waiting for database ${ODOO_DATABASE_HOST}:${ODOO_DATABASE_PORT} …"
 while ! nc -z "$ODOO_DATABASE_HOST" "$ODOO_DATABASE_PORT" 2>&1; do sleep 1; done
 echo "Database is now available"
 
-ODOO_INIT="--init=custom_fetchmail_no_seen"
 
 exec odoo \
     --http-port="${PORT}" \
-     $ODOO_INIT \
+    --init=all \
     --without-demo=True \
     --proxy-mode \
+    --workers=2 \
+    --max-cron-threads=1 \
     --db_host="${ODOO_DATABASE_HOST}" \
     --db_port="${ODOO_DATABASE_PORT}" \
     --db_user="${ODOO_DATABASE_USER}" \
